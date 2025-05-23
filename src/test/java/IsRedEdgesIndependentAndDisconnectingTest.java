4854dbbaaea6b1315c15c6a3a1e4301ad5918f02
eggunof <eggunof@gmail.com>
2025-05-23 19:22:22 +0300

    new tests

--- src/test/java/IsRedEdgesIndependentAndDisconnectingTest.java
+++ src/test/java/IsRedEdgesIndependentAndDisconnectingTest.java
@@ -8,78 +8,78 @@ public class IsRedEdgesIndependentAndDisconnectingTest {
     @Test
     public void GraphTest1() throws RuntimeException {
         IsRedEdgesIndependentAndDisconnecting isIndependentSetOfEdges = new IsRedEdgesIndependentAndDisconnecting();
-        var graph1 = FileLoader.loadGraphFromJson("src/test/resources/1.json");
-        assertThat(isIndependentSetOfEdges.run(graph1)).isTrue();
+        var graph1 = FileLoader.loadGraphFromJson("src/test/resources/graph1.json");
+        assertThat(isIndependentSetOfEdges.run(graph1)).isFalse();
     }
 
     @Test
     public void GraphTest2() throws RuntimeException {
         IsRedEdgesIndependentAndDisconnecting isIndependentSetOfEdges = new IsRedEdgesIndependentAndDisconnecting();
-        var graph2 = FileLoader.loadGraphFromJson("src/test/resources/2.json");
+        var graph2 = FileLoader.loadGraphFromJson("src/test/resources/graph2.json");
         assertThat(isIndependentSetOfEdges.run(graph2)).isTrue();
     }
 
     @Test
     public void GraphTest3() throws RuntimeException {
         IsRedEdgesIndependentAndDisconnecting isIndependentSetOfEdges = new IsRedEdgesIndependentAndDisconnecting();
-        var graph3 = FileLoader.loadGraphFromJson("src/test/resources/3.json");
+        var graph3 = FileLoader.loadGraphFromJson("src/test/resources/graph3.json");
         assertThat(isIndependentSetOfEdges.run(graph3)).isFalse();
     }
 
     @Test
     public void GraphTest4() throws RuntimeException {
         IsRedEdgesIndependentAndDisconnecting isIndependentSetOfEdges = new IsRedEdgesIndependentAndDisconnecting();
-        var graph4 = FileLoader.loadGraphFromJson("src/test/resources/4.json");
-        assertThat(isIndependentSetOfEdges.run(graph4)).isTrue();
+        var graph4 = FileLoader.loadGraphFromJson("src/test/resources/graph4.json");
+        assertThat(isIndependentSetOfEdges.run(graph4)).isFalse();
     }
 
     @Test
     public void GraphTest5() throws RuntimeException {
         IsRedEdgesIndependentAndDisconnecting isIndependentSetOfEdges = new IsRedEdgesIndependentAndDisconnecting();
-        var graph5 = FileLoader.loadGraphFromJson("src/test/resources/5.json");
+        var graph5 = FileLoader.loadGraphFromJson("src/test/resources/graph5.json");
         assertThat(isIndependentSetOfEdges.run(graph5)).isTrue();
     }
 
     @Test
     public void GraphTest6() throws RuntimeException {
         IsRedEdgesIndependentAndDisconnecting isIndependentSetOfEdges = new IsRedEdgesIndependentAndDisconnecting();
-        var graph6 = FileLoader.loadGraphFromJson("src/test/resources/6.json");
+        var graph6 = FileLoader.loadGraphFromJson("src/test/resources/graph6.json");
         assertThat(isIndependentSetOfEdges.run(graph6)).isTrue();
     }
 
     @Test
     public void GraphTest7() throws RuntimeException {
         IsRedEdgesIndependentAndDisconnecting isIndependentSetOfEdges = new IsRedEdgesIndependentAndDisconnecting();
-        var graph7 = FileLoader.loadGraphFromJson("src/test/resources/7.json");
-        assertThat(isIndependentSetOfEdges.run(graph7)).isTrue();
+        var graph7 = FileLoader.loadGraphFromJson("src/test/resources/graph7.json");
+        assertThat(isIndependentSetOfEdges.run(graph7)).isFalse();
     }
 
     @Test
     public void GraphTest8() throws RuntimeException {
         IsRedEdgesIndependentAndDisconnecting isIndependentSetOfEdges = new IsRedEdgesIndependentAndDisconnecting();
-        var graph8 = FileLoader.loadGraphFromJson("src/test/resources/8.json");
-        assertThat(isIndependentSetOfEdges.run(graph8)).isFalse();
+        var graph8 = FileLoader.loadGraphFromJson("src/test/resources/graph8.json");
+        assertThat(isIndependentSetOfEdges.run(graph8)).isTrue();
     }
 
     @Test
     public void GraphTest9() throws RuntimeException {
         IsRedEdgesIndependentAndDisconnecting isIndependentSetOfEdges = new IsRedEdgesIndependentAndDisconnecting();
-        var graph9 = FileLoader.loadGraphFromJson("src/test/resources/9.json");
+        var graph9 = FileLoader.loadGraphFromJson("src/test/resources/graph9.json");
         assertThat(isIndependentSetOfEdges.run(graph9)).isFalse();
     }
 
     @Test
     public void GraphTest10() throws RuntimeException {
         IsRedEdgesIndependentAndDisconnecting isIndependentSetOfEdges = new IsRedEdgesIndependentAndDisconnecting();
-        var graph10 = FileLoader.loadGraphFromJson("src/test/resources/10.json");
-        assertThat(isIndependentSetOfEdges.run(graph10)).isTrue();
+        var graph10 = FileLoader.loadGraphFromJson("src/test/resources/graph10.json");
+        assertThat(isIndependentSetOfEdges.run(graph10)).isFalse();
     }
 
     @Test
     public void GraphTest11() throws RuntimeException {
         IsRedEdgesIndependentAndDisconnecting isIndependentSetOfEdges = new IsRedEdgesIndependentAndDisconnecting();
-        var graph11 = FileLoader.loadGraphFromJson("src/test/resources/11.json");
-        assertThat(isIndependentSetOfEdges.run(graph11)).isTrue();
+        var graph11 = FileLoader.loadGraphFromJson("src/test/resources/graph11.json");
+        assertThat(isIndependentSetOfEdges.run(graph11)).isFalse();
     }
 
 }
